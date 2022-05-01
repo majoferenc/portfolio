75c747c25d865d5f7524caab3422505d3f9ec293
Marian-Ferenc2 <Marian.Ferenc2@ibm.com>
2022-05-01 22:02:37 +0200

    feat: gh-pages

--- src/pages/app/App.tsx
+++ src/pages/app/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import { Footer } from '../../components/footer/Footer'
 import { Header } from '../../components/header/Header'
 import { About } from '../about/About'
