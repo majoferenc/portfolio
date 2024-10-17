4c7cd86ca0826c8a57544e72ccc803bb16040141
Marian Ferenc <44170671+majoferenc@users.noreply.github.com>
2024-10-17 16:23:07 +0200

    Contact page (#10)
    * feat: contact page impl
    
    * fix: styling
    
    * refactor components

---
+++ src/components/SpotlightButton/SpotlightButton.tsx
@@ -0,0 +1,67 @@
+import { motion } from "framer-motion";
+import { useEffect, useRef } from "react";
+
+// ButtonWrapper component that accepts buttonText as a prop
+export function ButtonWrapper({ buttonText }: { buttonText: string }) {
+  return (
+    <div className="">
+      <SpotlightButton buttonText={buttonText} />
+    </div>
+  );
+}
+
+// SpotlightButton component accepts buttonText as a prop
+const SpotlightButton = ({ buttonText }: { buttonText: string }) => {
+  const btnRef = useRef<HTMLButtonElement | null>(null);
+  const spanRef = useRef<HTMLSpanElement | null>(null);
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      if (!btnRef.current || !spanRef.current) return; // Ensure the refs are not null
+
+      const { width } = btnRef.current.getBoundingClientRect();
+      const offset = e.offsetX;
+      const left = `${(offset / width) * 100}%`;
+      
+      // Check if the spanRef is valid before animating
+      if (spanRef.current) {
+        spanRef.current.animate({ left }, { duration: 250, fill: "forwards" });
+      }
+    };
+
+    const handleMouseLeave = () => {
+  
+    };
+
+    const btnElement = btnRef.current;
+    if (btnElement) {
+      btnElement.addEventListener("mousemove", handleMouseMove);
+      btnElement.addEventListener("mouseleave", handleMouseLeave);
+    }
+
+    return () => {
+      if (btnElement) {
+        btnElement.removeEventListener("mousemove", handleMouseMove);
+        btnElement.removeEventListener("mouseleave", handleMouseLeave);
+      }
+    };
+  }, []);
+
+  return (
+    <motion.button
+      whileTap={{ scale: 0.985 }}
+      ref={btnRef}
+      className="relative w-full max-w-xs overflow-hidden rounded-lg bg-slate-950 px-4 py-3 text-lg font-medium text-yellow-500"
+    >
+      <span className="pointer-events-none relative z-10 mix-blend-difference">
+        {buttonText} {/* Use the buttonText prop */}
+      </span>
+      <span
+        ref={spanRef}
+        className="pointer-events-none absolute left-[50%] top-[50%] h-32 w-32 -translate-x-[50%] -translate-y-[50%] rounded-full bg-orange-500 text-yellow-500  bg-gradient-to-r blur-2xl brightness-150 from-orange-600 via-yellow-600 to-orange-60"
+      />
+    </motion.button>
+  );
+};
+
+export default ButtonWrapper;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -14,7 +14,8 @@ import CKADCert from "../../images/ckad_cert.png";
 import OracleProfessionalCert from "../../images/oracle_java_8_prof_cert.png";
 import { WavyBackground } from "../../components/WavyBackground/WavyBackground";
 import { Scroll } from "../../components/Scroll/Scroll";
-
+import { ExpertiseCard } from "../../components/ExpertiseCard/ExpertiseCard";
+import { ByTheNumbersCard } from "../../components/ByTheNumbersCard/ByTheNumbersCard";
 
 const opacitySetter = (
   componentRef: RefObject<HTMLDivElement>,
@@ -33,6 +34,46 @@ export function Home() {
   const fullNammeText = "Ing. Marián Ferenc"
   const descriptionText =
     "Full Stack Developer With DevOps Skills from Košice, Slovakia.";
+  const byTheNumbersCard = [
+    {
+      number: 4,
+      text: "Spoken Languages"
+    },
+    {
+      number: 6,
+      text: "Customer facing Projects"
+    },
+    {
+      number: 7,
+      text: "Years of Experience"
+    },
+    {
+      number: 35,
+      text: "Certifications/Badges"
+    }
+  ];
+  const expertiseCards = [
+    {
+      description: 'Vue.js / React.js / Angular',
+      title: 'Frontend Development',
+      experience: '3 years of experience',
+    },
+    {
+      description: 'Spring Boot / Java EE / Node.js',
+      title: 'Backend development',
+      experience: '7 years of experience',
+    },
+    {
+      description: 'Kubernetes / Cloudification / Pipelining',
+      title: 'DevOps Engineer',
+      experience: '5 years of experience',
+    },
+    {
+      description: 'Cloud Architecture',
+      title: 'Application Architect',
+      experience: '2 years of experience',
+    },
+  ];
   const bodyRef = useRef<HTMLDivElement | null>(null);
   const nameRef = useRef<HTMLDivElement | null>(null);
   const descriptionRef = useRef<HTMLDivElement | null>(null);
@@ -95,7 +136,7 @@ export function Home() {
               >
                 <img
                   src={LinkedIn}
-                  className={`h-[4rem] m-0 ${hoverEffectClasses}`}
+                  className={`h-[4rem] m-0 ${hoverEffectClasses} bg-white-700`}
                   alt="LinkedIn link"
                 />
               </a>
@@ -150,42 +191,7 @@ export function Home() {
                   </div>
                 </div>
               </div>
-              <div className="mx-auto container md:-mt-28 -mt-20 flex justify-center items-center">
-                <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-2 gap-x-2 gap-y-2 lg:gap-x-6 md:gap-x-6 md:gap-y-6">
-                  <div className="flex justify-center flex-col items-center w-36 h-36 md:w-44 md:h-48 lg:w-56 lg:h-56 bg-white shadow rounded-2xl">
-                    <h2 className="lg:text-5xl md:text-4xl text-2xl font-extrabold leading-10 text-center text-gray-800">
-                      4
-                    </h2>
-                    <p className="mt-4 text-sm md:text-base lg:text-lg leading-none text-center text-gray-600">
-                      Spoken Languages
-                    </p>
-                  </div>
-                  <div className="flex justify-center flex-col items-center w-36 h-36 md:w-44 md:h-48 lg:w-56 lg:h-56 bg-white shadow rounded-2xl">
-                    <h2 className="lg:text-5xl md:text-4xl text-2xl font-extrabold leading-10 text-center text-gray-800">
-                      6
-                    </h2>
-                    <p className="mt-4 text-sm md:text-base lg:text-lg leading-none text-center text-gray-600">
-                      Customer facing Projects
-                    </p>
-                  </div>
-                  <div className="flex justify-center flex-col items-center w-36 h-36 md:w-44 md:h-48 lg:w-56 lg:h-56 bg-white shadow rounded-2xl">
-                    <h2 className="lg:text-5xl md:text-4xl text-2xl font-extrabold leading-10 text-center text-gray-800">
-                      7+
-                    </h2>
-                    <p className="mt-4 text-sm md:text-base lg:text-lg leading-none text-center text-gray-600">
-                      Years of Experience
-                    </p>
-                  </div>
-                  <div className="flex justify-center flex-col items-center w-36 h-36 md:w-44 md:h-48 lg:w-56 lg:h-56 bg-white shadow rounded-2xl">
-                    <h2 className="lg:text-5xl md:text-4xl text-2xl font-extrabold leading-10 text-center text-gray-800">
-                      35
-                    </h2>
-                    <p className="mt-4 text-sm md:text-base lg:text-lg leading-none text-center text-gray-600">
-                      Certifications/Badges
-                    </p>
-                  </div>
-                </div>
-              </div>
+              <ByTheNumbersCard cards={byTheNumbersCard} />
             </div>
           </div>
         </div>
@@ -240,252 +246,7 @@ export function Home() {
                   </div>
                 </div>
               </div>
-              <div className="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:px-20 px-10 py-20 gap-6">
-                <div>
-                  <div className="group w-full bg-white relative flex flex-col items-center hover:bg-yellow-400 cursor-pointer shadow-md md:p-12 p-4">
-                    <div className="text-gray-600 group-hover:text-white flex flex-col items-center">
-                      <svg
-                        width={26}
-                        height={27}
-                        viewBox="0 0 26 27"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <g clipPath="url(#clip0)">
-                          <path
-                            d="M25.2578 14.3309H19.2969C19.3988 9.55819 20.6309 9.01642 22.1785 8.86178L22.7753 8.78051V3.53242L22.0874 3.57292C20.0666 3.69783 17.8323 4.09805 16.3417 6.11965C15.035 7.89183 14.459 10.7871 14.459 15.2316V23.4673H25.2578V14.3309Z"
-                            fill="currentColor"
-                          />
-                          <path
-                            d="M11.48 23.4673V14.3309H5.59859C5.70049 9.55819 6.89283 9.01642 8.44042 8.86178L8.99749 8.78051V3.53242L8.34931 3.57292C6.32844 3.69783 4.07421 4.09805 2.5836 6.11965C1.27707 7.89183 0.681147 10.7871 0.681147 15.2316V23.4673H11.48Z"
-                            fill="currentColor"
-                          />
-                        </g>
-                        <defs>
-                          <clipPath id="clip0">
-                            <rect
-                              width="24.5767"
-                              height={27}
-                              fill="white"
-                              transform="translate(25.2578 27) rotate(-180)"
-                            />
-                          </clipPath>
-                        </defs>
-                      </svg>
-                      <p className="xl:w-80 text-base leading-normal text-center mt-4">
-                        Vue.js / React.js / Angular
-                      </p>
-                    </div>
-                    <div className="text-white group-hover:text-indigo-700 absolute bottom-0 -mb-6">
-                      <svg
-                        width={34}
-                        height={28}
-                        viewBox="0 0 34 28"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <g filter="url(#filter0_dd)">
-                          <path
-                            d="M17 19L28.2583 3.25H5.74167L17 19Z"
-                            fill="currentColor"
-                          />
-                        </g>
-                      </svg>
-                    </div>
-                  </div>
-                  <div className="flex flex-col items-center justify-center mt-10">
-                    <p className="text-base font-semibold leading-4 my-2 text-gray-800">
-                      Frontend Development
-                    </p>
-                    <p className="text-base leading-4 text-center text-gray-600">
-                      3 years of experience
-                    </p>
-                  </div>
-                </div>
-                <div>
-                  <div className="group w-full bg-white relative flex flex-col items-center hover:bg-yellow-400 cursor-pointer shadow-md md:p-12 p-4">
-                    <div className="text-gray-600 group-hover:text-white flex flex-col items-center">
-                      <svg
-                        width={26}
-                        height={27}
-                        viewBox="0 0 26 27"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <g clipPath="url(#clip0)">
-                          <path
-                            d="M25.2578 14.3309H19.2969C19.3988 9.55819 20.6309 9.01642 22.1785 8.86178L22.7753 8.78051V3.53242L22.0874 3.57292C20.0666 3.69783 17.8323 4.09805 16.3417 6.11965C15.035 7.89183 14.459 10.7871 14.459 15.2316V23.4673H25.2578V14.3309Z"
-                            fill="currentColor"
-                          />
-                          <path
-                            d="M11.48 23.4673V14.3309H5.59859C5.70049 9.55819 6.89283 9.01642 8.44042 8.86178L8.99749 8.78051V3.53242L8.34931 3.57292C6.32844 3.69783 4.07421 4.09805 2.5836 6.11965C1.27707 7.89183 0.681147 10.7871 0.681147 15.2316V23.4673H11.48Z"
-                            fill="currentColor"
-                          />
-                        </g>
-                        <defs>
-                          <clipPath id="clip0">
-                            <rect
-                              width="24.5767"
-                              height={27}
-                              fill="white"
-                              transform="translate(25.2578 27) rotate(-180)"
-                            />
-                          </clipPath>
-                        </defs>
-                      </svg>
-                      <p className="xl:w-80 text-base leading-normal text-center mt-4">
-                        Spring Boot / Java EE / Node.js
-                      </p>
-                    </div>
-                    <div className="text-white group-hover:text-indigo-700 absolute bottom-0 -mb-6">
-                      <svg
-                        width={34}
-                        height={28}
-                        viewBox="0 0 34 28"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <g filter="url(#filter0_dd)">
-                          <path
-                            d="M17 19L28.2583 3.25H5.74167L17 19Z"
-                            fill="currentColor"
-                          />
-                        </g>
-                      </svg>
-                    </div>
-                  </div>
-                  <div className="flex flex-col items-center justify-center mt-10">
-                    <p className="text-base font-semibold leading-4 my-2 text-gray-800">
-                      Backend development
-                    </p>
-                    <p className="text-base leading-4 text-center text-gray-600">
-                      7 years of experience
-                    </p>
-                  </div>
-                </div>
-                <div>
-                  <div className="group w-full bg-white relative flex flex-col items-center hover:bg-yellow-400 cursor-pointer shadow-md md:p-12 p-4">
-                    <div className="text-gray-600 group-hover:text-white flex flex-col items-center">
-                      <svg
-                        width={26}
-                        height={27}
-                        viewBox="0 0 26 27"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <g clipPath="url(#clip0)">
-                          <path
-                            d="M25.2578 14.3309H19.2969C19.3988 9.55819 20.6309 9.01642 22.1785 8.86178L22.7753 8.78051V3.53242L22.0874 3.57292C20.0666 3.69783 17.8323 4.09805 16.3417 6.11965C15.035 7.89183 14.459 10.7871 14.459 15.2316V23.4673H25.2578V14.3309Z"
-                            fill="currentColor"
-                          />
-                          <path
-                            d="M11.48 23.4673V14.3309H5.59859C5.70049 9.55819 6.89283 9.01642 8.44042 8.86178L8.99749 8.78051V3.53242L8.34931 3.57292C6.32844 3.69783 4.07421 4.09805 2.5836 6.11965C1.27707 7.89183 0.681147 10.7871 0.681147 15.2316V23.4673H11.48Z"
-                            fill="currentColor"
-                          />
-                        </g>
-                        <defs>
-                          <clipPath id="clip0">
-                            <rect
-                              width="24.5767"
-                              height={27}
-                              fill="white"
-                              transform="translate(25.2578 27) rotate(-180)"
-                            />
-                          </clipPath>
-                        </defs>
-                      </svg>
-                      <p className="xl:w-80 text-base leading-normal text-center mt-4">
-                        Kubernetes / Cloudification / Pipelining
-                      </p>
-                    </div>
-                    <div className="text-white group-hover:text-indigo-700 absolute bottom-0 -mb-6">
-                      <svg
-                        width={34}
-                        height={28}
-                        viewBox="0 0 34 28"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <g filter="url(#filter0_dd)">
-                          <path
-                            d="M17 19L28.2583 3.25H5.74167L17 19Z"
-                            fill="currentColor"
-                          />
-                        </g>
-                      </svg>
-                    </div>
-                  </div>
-                  <div className="flex flex-col items-center justify-center mt-10">
-                    <p className="text-base font-semibold leading-4 my-2 text-gray-800">
-                      DevOps Enginner
-                    </p>
-                    <p className="text-base leading-4 text-center text-gray-600">
-                      5 years of experience
-                    </p>
-                  </div>
-                </div>
-                <div>
-                  <div className="group w-full bg-white relative flex flex-col items-center hover:bg-yellow-400 cursor-pointer shadow-md md:p-12 p-4">
-                    <div className="text-gray-600 group-hover:text-white flex flex-col items-center">
-                      <svg
-                        width={26}
-                        height={27}
-                        viewBox="0 0 26 27"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <g clipPath="url(#clip0)">
-                          <path
-                            d="M25.2578 14.3309H19.2969C19.3988 9.55819 20.6309 9.01642 22.1785 8.86178L22.7753 8.78051V3.53242L22.0874 3.57292C20.0666 3.69783 17.8323 4.09805 16.3417 6.11965C15.035 7.89183 14.459 10.7871 14.459 15.2316V23.4673H25.2578V14.3309Z"
-                            fill="currentColor"
-                          />
-                          <path
-                            d="M11.48 23.4673V14.3309H5.59859C5.70049 9.55819 6.89283 9.01642 8.44042 8.86178L8.99749 8.78051V3.53242L8.34931 3.57292C6.32844 3.69783 4.07421 4.09805 2.5836 6.11965C1.27707 7.89183 0.681147 10.7871 0.681147 15.2316V23.4673H11.48Z"
-                            fill="currentColor"
-                          />
-                        </g>
-                        <defs>
-                          <clipPath id="clip0">
-                            <rect
-                              width="24.5767"
-                              height={27}
-                              fill="white"
-                              transform="translate(25.2578 27) rotate(-180)"
-                            />
-                          </clipPath>
-                        </defs>
-                      </svg>
-                      <p className="xl:w-80 text-base leading-normal text-center mt-4">
-                        Cloud Architecture
-                      </p>
-                    </div>
-                    <div className="text-white group-hover:text-indigo-700 absolute bottom-0 -mb-6">
-                      <svg
-                        width={34}
-                        height={28}
-                        viewBox="0 0 34 28"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <g filter="url(#filter0_dd)">
-                          <path
-                            d="M17 19L28.2583 3.25H5.74167L17 19Z"
-                            fill="currentColor"
-                          />
-                        </g>
-                      </svg>
-                    </div>
-                  </div>
-                  <div className="flex flex-col items-center justify-center mt-10">
-                    <p className="text-base font-semibold leading-4 my-2 text-gray-800">
-                      Application Architect
-                    </p>
-                    <p className="text-base leading-4 text-center text-gray-600">
-                      2 years of experience
-                    </p>
-                  </div>
-                </div>
-              </div>
+              <ExpertiseCard cards={expertiseCards} />
             </div>
           </div>
         </div>
