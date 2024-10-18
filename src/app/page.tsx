0c8cebd3ebf6e299358b59294c815b8e15fdbcb2
Marian Ferenc <marianferenc59@gmail.com>
2024-10-18 18:08:32 +0200

    fix: credly button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -323,14 +323,21 @@ export default function Page() {
                   />
                 </div>
               </div>
-              <a
-                href="https://www.credly.com/users/marian-ferenc/badges"
-                target="_blank"
-                type="submit"
-                className={`text-white rounded font-bold py-2 px-4 m-4 text-sm bg-gray-900 ${hoverEffectClasses}`}
-              >
-                Credly
-              </a>
+              <div className="flex items-center justify-center">
+                <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+                  <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+                  <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+                    <a
+                      href="https://www.credly.com/users/marian-ferenc/badges"
+                      target="_blank"
+                      type="submit"
+                      className={`mx-auto text-white rounded font-bold py-2 px-4 m-4 text-sm bg-gray-900`}
+                    >
+                      Credly
+                    </a>
+                  </span>
+                </button>
+              </div>
             </div>
           </div>
         </div>
