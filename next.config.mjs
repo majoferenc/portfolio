0837471f99a7331412530323ed9a81d9703aba72
Marian Ferenc <marianferenc59@gmail.com>
2025-12-12 19:38:53 +0100

    fix: images

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ import createMDX from "@next/mdx";
 /** @type {import('next').NextConfig} */
 const nextConfig = { 
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
